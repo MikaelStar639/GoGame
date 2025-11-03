39fb4c92f3e2dc3e42ab3d60d36828bd7e7128c7
mikael <mikaelvo639@gmail.com>
2025-11-03 13:10:56 +0700

    Add simple Button class

---
+++ include/UI/Button.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <string>
+#include "Input/Mouse.hpp"
+
+//to edit text inside the button
+void alignText(sf::Text &text, float horizontal, float vertical);
+
+class Button {
+private:
+    sf::RectangleShape shape;
+    sf::Text           text;
+
+public:
+    sf::Vector2f size         = {200.f, 100.f};
+    sf::Vector2f position     = {0.f,  0.f};
+    std::string  string       = "Button";
+    float        text_scale   = 0.5f;
+    float        text_align_v = 0.6f;
+
+    //state
+    bool isOver          = false;
+    bool isPressed       = false;
+    bool onPress         = false;
+    bool onRelease       = false;
+
+    sf::Color default_shape = sf::Color(0, 170, 255);
+    sf::Color default_text  = sf::Color::White;
+    sf::Color over_shape    = default_text;
+    sf::Color over_text     = default_shape;
+    sf::Color pressed_shape = sf::Color(64, 64, 127);
+    sf::Color pressed_text  = sf::Color::White;
+
+    Button(const sf::Font &font);
+
+    void update(Mouse &mouse);
+    void draw(sf::RenderWindow& window);
+};
---
+++ src/UI/Button.cpp
@@ -0,0 +1,79 @@
+#include "UI/Button.hpp"
+
+void alignText(sf::Text &text, float horizontal, float vertical){
+    sf::FloatRect bounds = text.getLocalBounds();
+
+    float x = bounds.position.x + bounds.size.x * horizontal;
+    float y = text.getCharacterSize() * vertical;
+
+    text.setOrigin({x, y});
+}
+
+Button::Button(const sf::Font &font) : text(font) {}
+
+void Button::update(Mouse &mouse){
+    
+    //reset states
+    onPress   = false;
+    onRelease = false;
+
+    //mouse
+    if (shape.getGlobalBounds().contains(mouse.position)){
+        if (mouse.onPress){
+            //first click
+            onPress   = true;
+            isPressed = true;
+        }
+        if (mouse.onRelease && isPressed){
+            //first release
+            onRelease = true;
+        }
+
+        if (mouse.isPressed && isPressed){
+            //if already clicked inside the button
+            isPressed = true;
+        }
+
+        isOver = true;
+    }
+    else{
+        isOver    = false;
+        isPressed = false;
+    }
+    
+    if (!mouse.isPressed){
+        isPressed = false;
+    }
+}
+
+//draw
+void Button::draw(sf::RenderWindow& window){
+
+    //shape
+    shape.setSize(size);
+    shape.setOrigin(shape.getGeometricCenter());
+    shape.setPosition(position);
+    window.draw(shape);
+
+    //text
+    text.setString(string);
+    text.setCharacterSize(size.y * text_scale);
+    text.setPosition(position);
+    alignText(text, 0.5f, text_align_v);
+
+    //color
+    if (isPressed){
+        shape.setFillColor(pressed_shape);
+        text.setFillColor(pressed_text);
+    }
+    else if (isOver){
+        shape.setFillColor(over_shape);
+        text.setFillColor(over_text);
+    }
+    else{
+        shape.setFillColor(default_shape);
+        text.setFillColor(default_text);
+    }
+
+    window.draw(text);
+}
