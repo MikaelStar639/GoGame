e82c889f0d216abf31926e60c17a302f79ba8452
mikael <mikaelvo639@gmail.com>
2025-11-04 23:52:22 +0700

    window state update

---
+++ include/Window-State/Homescreen.hpp
@@ -0,0 +1,23 @@
+#include "Game.hpp"
+#include "UI/Button.hpp"
+#include "UI/RoundRectShape.hpp"
+
+class Homescreen{
+private:
+    Button playButton;
+    Button resumeButton;
+    Button settingButton;
+    Button exitButton;
+
+    sf::RenderWindow &window;
+
+public:
+    Homescreen(sf::Font &font, sf::RenderWindow &window);
+
+    Game::windowState nextState = Game::windowState::Homescreen;
+
+    void setBackground(sf::Sprite &backgroundSprite);
+    void updateButton(Mouse &mouse);
+    void drawButton();
+    void run();
+};
